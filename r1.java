13a3a0dd63c5d8267e4935127e8dd6c95a771e47
jfsjdrajeshri <129038770+jfsjdrajeshri@users.noreply.github.com>
2023-03-28 10:28:10 +0530

    Add files via upload

---
+++ r1.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+
+class r1
+
+{
+
+public static void main(String args[])
+
+{
+
+System.out.println("first  Random number is :" +Math.random());
+
+
+
+System.out.println("second Random Number is :" +Math.random());
+
+}
+
+}
